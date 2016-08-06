bb9954f2ca2d2812e2c0789db588cecaec506a86
Mika'il Khan <co.kleisli@gmail.com>
2016-08-06 12:21:35 +0100

    completed step-1

---
+++ src/main/scala/gov/hmrc/checkout/CheckOut.scala
@@ -0,0 +1,14 @@
+package gov.hmrc.checkout
+
+import gov.hmrc.checkout.Cart._
+
+object CheckOut {
+  def main(args: Array[String]) {
+    /*
+      A rudimentary console tool which accepts shopping items (space separated) e.g Apple Apple Orange Orange.
+      On pressing enter in the console, the total cost of the items will be displayed
+     */
+    val shopping = io.StdIn.readLine().split(' ').toList
+    println(totalCost(Cart(shopping)))
+  }
+}
